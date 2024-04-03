188296f007675e93a4af7328c384b3970782abe3
Alexandre Costa <alexandresantoscosta@gmail.com>
2024-04-03 07:42:15 -0300

    Bumped version to latest NVDA 2024.1

--- buildVars.py
+++ buildVars.py
@@ -35,7 +35,7 @@ Control timer and stopwatch from anywhere using NVDA commands."""),
     # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
     "addon_minimumNVDAVersion": "2022.1",
     # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-    "addon_lastTestedNVDAVersion": "2023.1",
+    "addon_lastTestedNVDAVersion": "2024.1",
     # Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
     "addon_updateChannel": None,
 }
